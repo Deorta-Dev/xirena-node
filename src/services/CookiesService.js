86ccf350e425dc2bcdedcf23559ca28daa314aa5
Manuel De Orta <manueldeortac@gmail.com>
2021-02-17 18:55:57 -0500

    Mejoras de indexacion de CookiesService.js

--- src/services/CookiesService.js
+++ src/services/CookiesService.js
@@ -12,8 +12,10 @@ module.exports = {
                 let cookiesMap = {};
                 if (typeof $request.headers.cookie === 'string')
                     $request.headers.cookie.split(';').forEach(stringCookie => {
-                        [n, v] = stringCookie.split('=');
-                        cookiesMap[n] = v;
+                        [c, v] = stringCookie.split('=');
+                        while (c.startsWith(' ')) c = c.slice(1);
+                        while (c.endsWith(' ')) c = c.slice(0, -1);
+                        cookiesMap[c] = v;
                     });
                 return decodeURIComponent(cookiesMap[name]);
             }
